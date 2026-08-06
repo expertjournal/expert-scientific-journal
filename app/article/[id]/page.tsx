b5d3f42888e7b34dc3b254d5c266e45f5d4bed78
Dilshodbek Nazarov <dilshodbekn188@gmail.com>
2026-08-06 12:25:28 +0500

    Fix Next.js App Router dynamic route conflict by removing app/article/[id] stub in favor of app/article/[slug]

--- app/article/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { use } from "react";
-import ArticleDetailPage from "../page";
-
-export default function DynamicArticlePage({ params }: { params: Promise<{ id: string }> }) {
-  const resolvedParams = use(params);
-  return <ArticleDetailPage />;
-}
